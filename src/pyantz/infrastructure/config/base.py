4b17e41e4b40ddd1ff157c35ecd419e52a0b8df5
igreen1 <dev@iangreen.dev>
2025-02-17 19:49:04 -0500

    Give up on validation of job params

--- src/pyantz/infrastructure/config/base.py
+++ src/pyantz/infrastructure/config/base.py
@@ -247,45 +247,12 @@ class InitialConfig(BaseModel, frozen=True):
     logging_config: LoggingConfig = LoggingConfig()
 
 
-InitialConfig.model_rebuild()
-Config.model_rebuild()
-PipelineConfig.model_rebuild()
-JobConfig.model_rebuild()
-SubmitterJobConfig.model_rebuild()
-MutableJobConfig.model_rebuild()
-
-def mutable_job(
-    fn: Callable[
-        [ParametersType, Mapping[str, PrimitiveType], logging.Logger],
-        tuple[Status, Mapping[str, PrimitiveType]],
-    ],
-) -> MutableJobFunctionType:
-    """Wrap a mutable job to
-    1. Allow it to accept variable args if a user incorrectly marks job
-    2. Allow for type checking in the pydantic model
-    """
-
-    @validate_call
-    @wraps(fn)
-    def _mutable_job(
-        params: ParametersType,
-        variables: Mapping[str, PrimitiveType],
-        logger: logging.Logger,
-        *_: Any,
-    ) -> tuple[Status, Mapping[str, PrimitiveType]]:
-        return fn(params, variables, logger)
-
-    setattr(_mutable_job, _SPECIAL_ATTRIBUTE_NAME, "mutable")
-    return _mutable_job
-
-
 def submitter_job(fn: SubmitterJobFunctionType) -> SubmitterJobFunctionType:
     """Wrap a submitter job to
     1. Allow it to accept variable args if a user incorrectly marks job
     2. Allow for type checking in the pydantic model
     """
 
-    @validate_call
     @wraps(fn)
     def _submitter_job(
         params: ParametersType,
@@ -307,7 +274,6 @@ def simple_job(
     2. Allow for type checking in the pydantic model
     """
 
-    @validate_call
     @wraps(fn)
     def _simple_job(
         params: ParametersType,
