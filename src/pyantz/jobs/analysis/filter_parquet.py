0a58e129a655d5ac8f96c7b471b5c33c192364dd
Ian <dev@iangreen.dev>
2025-07-15 21:17:15 -0400

    Handle multiple filters at once

--- src/pyantz/jobs/analysis/filter_parquet.py
+++ src/pyantz/jobs/analysis/filter_parquet.py
@@ -2,12 +2,13 @@
 
 import logging
 import operator
-import os
-from typing import Any, Callable, Literal, TypeAlias
+
+from typing import Callable, Literal, TypeAlias
+from functools import reduce
+import operator
 
 import polars as pl
-from pydantic import BaseModel, BeforeValidator, FilePath, field_validator
-from typing_extensions import Annotated
+from pydantic import BaseModel
 
 import pyantz.infrastructure.config.base as config_base
 from pyantz.infrastructure.core.status import Status
@@ -15,9 +16,19 @@ from pyantz.infrastructure.core.status import Status
 FilterType: TypeAlias = list[
     list[
         tuple[
-            str,
+            int | float | bool | str,
             Literal["==", "=", "!=", ">", ">=", "<", "<="],
             int | float | bool | str,
+        ]
+    ]
+]
+
+FilterTypeAfter: TypeAlias = list[
+    list[
+        tuple[
+            int | float | bool | str | pl.Expr,
+            Callable[..., bool],
+            int | float | bool | str | pl.Expr,
         ]
     ]
 ]
@@ -28,9 +39,7 @@ class FilterParquetParameters(BaseModel, frozen=True):
 
     input_file: str
     output_file: str
-    left: str
-    op: Literal["==", "=", "~=", "!=", ">", ">=", "<", "<="]
-    right: str | int | float | bool
+    filters: FilterType
 
 
 _operator_mapping: dict[str, Callable[..., bool]] = {
@@ -45,52 +54,47 @@ _operator_mapping: dict[str, Callable[..., bool]] = {
 }
 
 
-class FilterParquetParametersAfter(BaseModel, frozen=True):
-    """Parameters for filter_parquet"""
-
-    input_file: FilePath
-    output_file: Annotated[
-        str,
-        BeforeValidator(lambda x: x if os.path.exists(os.path.dirname(x)) else None),
-    ]
-    left: str | int | float | bool
-    op: Callable[..., bool]
-    right: str | int | float | bool
-
-    @field_validator("op", mode="before")
-    @classmethod
-    def _op_transform(cls, value: Any) -> Any:
-        """Transfor operator if its a string"""
-        if isinstance(value, str):
-            return _operator_mapping.get(value, value)
-        return value
-
-
 @config_base.simple_job(FilterParquetParameters)
 def filter_parquet(
-    parameters: config_base.ParametersType, logger: logging.Logger
+    parameters: config_base.ParametersType,
+    logger: logging.Logger,  # pylint: disable=unused-argument
 ) -> Status:
     """Filter the parquet file down"""
 
-    params = FilterParquetParametersAfter.model_validate(parameters)
+    params = FilterParquetParameters.model_validate(parameters)
 
     lazy_frame = pl.scan_parquet(params.input_file)
-
     columns: list[str] = lazy_frame.collect_schema().names()
 
-    lhs = (
-        pl.col(params.left)
-        if isinstance(params.left, str) and params.left in columns
-        else params.left
-    )
-    rhs = (
-        pl.col(params.right)
-        if isinstance(params.right, str) and params.right in columns
-        else params.right
+    def get_as_col_if_possible[T](col_name: T) -> T | pl.Expr:
+        return (
+            pl.col(col_name)
+            if isinstance(col_name, str) and col_name in columns
+            else col_name
+        )
+
+    filters: bool | pl.Expr = reduce(
+        operator.or_,
+        [
+            reduce(
+                operator.and_,
+                [
+                    _operator_mapping[op](
+                        get_as_col_if_possible(lhs),
+                        get_as_col_if_possible(rhs),
+                    )
+                    for lhs, op, rhs in or_predicate
+                ],
+            )
+            for or_predicate in params.filters
+        ],
     )
 
-    pl.scan_parquet(params.input_file).filter(params.op(lhs, rhs)).sink_parquet(
-        params.output_file
-    )
-
+    try:
+        lazy_frame.filter(filters).sink_parquet(
+            params.output_file
+        )
+    except Exception as exc:
+        logger.error('Unable to filter: ', exc_info=exc)
+        return Status.ERROR
     return Status.SUCCESS
