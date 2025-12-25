c3b7afa0030eac490cbfe56c3e33e9920cd15070
igreen1 <iangreen2.00@gmail.com>
2025-12-25 11:12:32 -0800

    Better use type params

--- src/pyantz/__init__.py
+++ src/pyantz/__init__.py
@@ -4,18 +4,20 @@ It is useful if you want to quickly run some small things locally or on a
 slurm cluster without setting up specialized worker nodes.
 """
 
+from typing import Any as _Any
+
 from pyantz.infrastructure.config import AnyRunner as _AnyRunner
 from pyantz.infrastructure.config import InitialConfig as _InitialConfig
 from pyantz.infrastructure.runner import start_local as _start_local
 
 
-def start(config: _InitialConfig[_AnyRunner]) -> None:
+def start(config: _InitialConfig[_AnyRunner] | dict[str, _Any]) -> None:
     """Start running the various jobs.
 
     This is a blocking operation if running locally.
     """
     if not isinstance(config, _InitialConfig):  # pyright: ignore[reportUnnecessaryIsInstance]
-        config = _InitialConfig.model_validate(config)
+        config = _InitialConfig[_AnyRunner].model_validate(config)
     if config.submitter.type_ == "local_proc":
         # start local runner
         _start_local(config)
