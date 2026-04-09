4be1804db51c5b731e968ebc1bf59c3576b46433
ian <dev@iangreen.dev>
2026-04-08 21:20:55 -0400

    additional test

--- tests/config/test_variable_resolution.py
+++ tests/config/test_variable_resolution.py
@@ -22,3 +22,20 @@ def test_variable_variables() -> None:
     assert result == {
         "run_dir": "output/run_1/hello"
     }
+
+
+def test_double_nested_variables() -> None:
+    """Test the variables can contain variables and it will resolve properly."""
+    variables: dict[str, Any] = {
+        "output_dir": "output/run_%{pipeline_id}",
+        "pipeline_id": "a_%{run_id}",
+        "run_id": 10
+    }
+
+    parameters = {"run_dir": "%{output_dir}/hello"}
+
+    result, _ = resolve_parameters(parameters, variables=variables)
+
+    assert result == {
+        "run_dir": "output/run_a_10/hello"
+    }
